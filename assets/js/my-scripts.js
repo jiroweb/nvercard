a660304ca634f4d2d37dc6f9669a21dfbd8d2fe4
HermineHoromyan <hermine.horomyan@gmail.com>
2020-07-23 03:08:27 +0400

    new changes

--- assets/js/my-scripts.js
+++ assets/js/my-scripts.js
@@ -68,5 +68,43 @@ function drop() {
     body.classList.remove("adding-file");
 }
 
+//   //  ================ -RANGE-JS-START- ================
+ 
+ 
+
+$('#rangeCalc').on('input', function () {
+    this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);
+    let value = (this.value/parseInt(this.max))*100
+    var children = this.parentNode.childNodes[1].childNodes;
+    children[1].style.width=value+'%';
+    children[5].style.left=value+'%';
+    children[7].style.left=value+'%';children[11].style.left=value+'%';
+    children[11].childNodes[1].innerHTML=this.value;
+})
+$('#range-calc__second').on('input', function () {
+     this.value=Math.max(this.value,this.parentNode.childNodes[3].value-(-1));
+    let value = (this.value/parseInt(this.max))*100
+    var children = this.parentNode.childNodes[1].childNodes;
+    children[3].style.width=(100-value)+'%';
+    children[5].style.right=(100-value)+'%';
+    children[9].style.left=value+'%';children[13].style.left=value+'%';
+    children[13].childNodes[1].innerHTML=this.value;
+})
+
+
+//  ================ -RANGE-JS-END- ================
+
+// function for play video
+$('.video-play__bg').click(function () {
+var this_prt = $(this).parent()
+var parent_video = $(this_prt)
+var icon_video = $(parent_video).find(".video-play__bg")
+var video = $(parent_video).find(".video")
+$(video).trigger('play')
+$(video).attr("controls", "controls")
+$(icon_video).fadeOut()
+});
+
+
 
 
